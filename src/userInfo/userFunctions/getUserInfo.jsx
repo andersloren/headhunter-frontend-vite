1fffe58e008188e94d38d813f1ea4234b5662c99
Liebniz <anders.leif.loren@liebniz.com>
2024-07-17 14:29:38 +0200

    refactor: userInfo functions updated to meet #58 requirements

--- src/userInfo/userFunctions/getUserInfo.jsx
+++ src/userInfo/userFunctions/getUserInfo.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export async function getUserInfo(email, setName, setOrganization) {
-  const url = `http://localhost:8080/api/v1/userInfo/getUserInfo/${email}`;
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+  const url = `${apiUrl}/api/v1/userInfo/getUserInfo/${email}`;
+  console.log(apiUrl);
 
   try {
     const response = await axios.get(url, {
--- src/userInfo/userFunctions/updateUserInfo.jsx
+++ src/userInfo/userFunctions/updateUserInfo.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export async function updateUserInfo(email, name, organization) {
-  const url = `http://localhost:8080/api/v1/userInfo/updateUserInfo/${email}`;
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+  const url = `${apiUrl}/api/v1/userInfo/updateUserInfo/${email}`;
+  console.log(apiUrl);
 
   try {
     const response = await axios.put(
